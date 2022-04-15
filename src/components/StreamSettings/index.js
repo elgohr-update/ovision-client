3f8b675d852d05f9d625f6ad0cf77c9fecec9fee
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2022-04-15 17:02:06 +0500

    Added New Component
    Added StreamSettings

---
+++ src/components/StreamSettings/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+export const StreamSettings = () => {
+    return (
+      <div className="StreamSettings h-100 mt-3">
+        <h5 className="SteamSettings__title">Way</h5>
+      </div>
+    );
+  };
+  
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import api from '../../api';
+import { StreamSettings } from '../../components/StreamSettings';
 import { UserParams } from '../../components/UserParams';
 import { VideoStream } from '../../components/VideoStream';
 import { useLoader } from '../../hooks/useLoader';
@@ -25,10 +26,13 @@ export const Home = () => {
   return (
     <div className="Home">
       <Row>
+        <Col xl={3}>
+          <StreamSettings/>
+        </Col>
         <Col xs={6}>
           <VideoStream />
         </Col>
-        <Col xs={6}>
+        <Col xs={3}>
           <UserParams />
         </Col>
       </Row>
